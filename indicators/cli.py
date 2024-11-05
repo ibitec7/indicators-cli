fe68e91269b7c3a742622566724225ed48bb9ec7
ibitec7 <syed.ibrahim.omer.2@gmail.com>
2024-11-06 01:56:39 +0800

    CLI arguments issue

--- indicators/cli.py
+++ indicators/cli.py
@@ -3,11 +3,11 @@ from indicators import indicators  # Import your indicators functions
 
 @click.command()
 @click.argument("ticker")
-@click.argument("period")
+@click.option("-p", "--period", default="5y", help="Period of Stock data")
 @click.option("-o", "--output", default="indicators.csv", help="Output CSV file name")
 def main(ticker, period, output):
     """Fetch stock indicators for a given TICKER and save to a CSV file."""
-    indicators.calculate_indicators(ticker, output, period)
+    indicators.calculate_indicators(ticker, period, output)
     click.echo(f"Indicators saved to {output} for {ticker} over {period}")
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="indicators-cli",                     # Name of the package
-    version="0.1.0",
+    name="indicators-cli",                   
+    version="0.1.2",
     author="Syed Ibrahim Omer",
     author_email="syed.ibrahim.omer.2@gmail.com",
     description="CLI tool to calculate stock indicators",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/ibitec7/indicators-cli",  # GitHub URL
-    packages=find_packages(),                  # Automatically find packages
+    url="https://github.com/ibitec7/indicators-cli",
+    packages=find_packages(),                
     install_requires=[
         "pandas",
         "numpy",
         "yfinance",
-        "click"                                # Since you’re using Click for CLI
+        "click"                               
     ],
     entry_points={
         'console_scripts': [
-            'indicators=cli:main',             # Command name -> entry function
+            'indicators=indicators.cli:main',            
         ],
     },
     classifiers=[
