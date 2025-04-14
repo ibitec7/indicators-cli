32fec13c840ba0733f496c3270b1b8931795d3ef
ibitec7 <syed.ibrahim.omer.2@gmail.com>
2025-04-14 20:44:04 +0800

    Updated version information

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="indicators-cli",                   
-    version="1.2.1",
+    version="1.2.8",
     author="Syed Ibrahim Omer",
     author_email="syed.ibrahim.omer.2@gmail.com",
     description="CLI tool to calculate stock indicators",
